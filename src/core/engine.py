84b52fc39819614f506daa9d22246adb68231730
Aleksei Yerokhin <aleksei.yerokhin@gmail.com>
2026-09-22 15:56:35 +0300

    Guard album/artist responses against stale requests

--- src/core/engine.py
+++ src/core/engine.py
@@ -14,6 +14,8 @@ class BandcampEngine(QObject):
         super().__init__(parent)
         self._api = BandcampAPI()
         self._search_id = 0
+        self._artist_id = 0
+        self._album_id = 0
 
     def cleanup(self):
         pass
@@ -24,12 +26,16 @@ class BandcampEngine(QObject):
         threading.Thread(target=self._search_worker, args=(query, search_id), daemon=True).start()
 
     def get_artist_data(self, band_id):
-        threading.Thread(target=self._artist_worker, args=(band_id,), daemon=True).start()
+        self._artist_id += 1
+        artist_id = self._artist_id
+        threading.Thread(target=self._artist_worker, args=(band_id, artist_id), daemon=True).start()
 
     def get_album_data(self, band_id, tralbum_id, tralbum_type="a"):
+        self._album_id += 1
+        album_id = self._album_id
         threading.Thread(
             target=self._album_worker,
-            args=(band_id, tralbum_id, tralbum_type),
+            args=(band_id, tralbum_id, tralbum_type, album_id),
             daemon=True,
         ).start()
 
@@ -44,16 +50,24 @@ class BandcampEngine(QObject):
             else:
                 self.search_results_ready.emit(True, results)
 
-    def _artist_worker(self, band_id):
+    def _artist_worker(self, band_id, artist_id):
         try:
             data = self._api.band_details(band_id)
-            self.artist_data_ready.emit(True, data)
         except BandcampAPIError:
-            self.artist_data_ready.emit(False, {})
+            data = None
+        if artist_id == self._artist_id:
+            if data is None:
+                self.artist_data_ready.emit(False, {})
+            else:
+                self.artist_data_ready.emit(True, data)
 
-    def _album_worker(self, band_id, tralbum_id, tralbum_type):
+    def _album_worker(self, band_id, tralbum_id, tralbum_type, album_id):
         try:
             data = self._api.tralbum_details(band_id, tralbum_id, tralbum_type)
-            self.album_data_ready.emit(True, data)
         except BandcampAPIError:
-            self.album_data_ready.emit(False, {})
+            data = None
+        if album_id == self._album_id:
+            if data is None:
+                self.album_data_ready.emit(False, {})
+            else:
+                self.album_data_ready.emit(True, data)
